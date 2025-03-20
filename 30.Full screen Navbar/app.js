f680fb2d191dbfe610988ce2fa8c641a0498de92
Sarvesh23923 <sarvesh23923@gmail.com>
2025-03-20 12:04:42 +0530

    Full screen Nav

---
+++ 30.Full screen Navbar/app.js
@@ -0,0 +1,16 @@
+const menu = document.querySelector('.menu');
+const links = document.querySelectorAll('.menu li');
+const highlight = document.querySelector('.highlight');
+const button = document.querySelector('.btn');
+
+button.addEventListener('click', function () {
+    if(this.dataset.open === 'close'){
+        menu.style.clipPath = `circle(100% at 50% 50%)`;
+        this.dataset.open = 'open'
+    }
+    else{
+        menu.style.clipPath = '';
+        this.dataset.open = 'close'
+        highlight.style = '';
+    }
+})
